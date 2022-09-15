5ac2630b91aabd0523a8032b5d5bfe6e96ec1634
Hakan Öcal <hknocal@outlook.com>
2022-09-15 14:11:27 +0200

    added return message to user if search parameter is invalid

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -43,9 +43,11 @@ public class Main {
             } else if (userChoice == 2) { // print db
                 data.printDB();
             } else if (userChoice == 3) { // search function here
-                System.out.println("Enter valid search parameter (only name or the known name of the SuperHero");
+                System.out.println("Enter valid search parameter (only name or the known name of the SuperHero)");
                 String name = sc.next();
                 SuperHero s = data.findHero(name);
+                if (s == null)
+                    System.out.println("No SuperHero was found based on your search parameter.");
                 if (s != null)
                     System.out.println(s);
             }
