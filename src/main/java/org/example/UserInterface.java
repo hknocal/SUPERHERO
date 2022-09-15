7a69a74dbb3500bc5c10c48ca3c04b2382f1ee32
Hakan Öcal <hknocal@outlook.com>
2022-09-15 14:53:06 +0200

    added new userinterface class, moved main code, so that the main class solely initiates ui load

---
+++ src/main/java/org/example/UserInterface.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class UserInterface {
+    public void startUI() {
+        Scanner sc = new Scanner(System.in);
+        int userChoice;
+
+        // User interface
+        Database data = new Database();
+        do {
+            System.out.println("""
+                    Welcome to SuperHeroesDB!
+                    1. Create SuperHero
+                    2. Access the SuperHeroDB
+                    3. Search for SuperHero
+                    9. Terminate
+                    """);
+            userChoice = sc.nextInt();
+            sc.nextLine();
+
+            if (userChoice == 1) {
+                System.out.println("Enter the real name of your SuperHero");
+                String realName = sc.nextLine();
+                System.out.println("Enter the known name of your SuperHero");
+                String heroName = sc.nextLine();
+                System.out.println("Enter the special power of your SuperHero");
+                String heroPower = sc.nextLine();
+                System.out.println("Enter the strength of your SuperHero");
+                double heroStrength = sc.nextDouble();
+                System.out.println("Enter the creation year of your SuperHero");
+                int creationYear = sc.nextInt();
+                System.out.println("Is your SuperHero a human? Anser with 'YES'or 'NO'");
+                String isHumanString = sc.next();
+                boolean isHuman = false;
+
+                if (isHumanString.equalsIgnoreCase("YES")) {
+                    isHuman = true;
+
+                    data.addHero(realName, heroName, heroPower, heroStrength, creationYear, isHuman);
+                }
+
+            } else if (userChoice == 2) { // print db
+                data.printDB();
+            } else if (userChoice == 3) { // search function here
+                System.out.println("Enter valid search parameter (only name or the known name of the SuperHero)");
+                String name = sc.next();
+                SuperHero s = data.findHero(name);
+                if (s == null)
+                    System.out.println("No SuperHero was found based on your search parameter.");
+                if (s != null)
+                    System.out.println(s);
+            }
+
+        } while (userChoice != 9);
+    }
+}
